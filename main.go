d487a983a96c4c65b76bf0005c7e8862e2b2d127
Denis-Azarov <denis_azarov@xa.epicgames.com>
2024-07-27 00:42:04 +0100

    code(http): add simple mux and webserver

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Snippetworld!"))
+}
 
 func main() {
-	fmt.Println("Hello Snippetworld!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("start listening on port :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
