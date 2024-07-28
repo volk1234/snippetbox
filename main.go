2588e612864bdf36aae62818a356f472878804e9
Denis-Azarov <denis_azarov@xa.epicgames.com>
2024-07-28 01:28:02 +0100

    code(http): add 404 page for home

--- main.go
+++ main.go
@@ -6,12 +6,26 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello Snippetworld!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Showing the snippet!"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create super snippet here!"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/view", snippetView)
+	mux.HandleFunc("/create", snippetCreate)
 
 	log.Print("start listening on port :4000")
 	err := http.ListenAndServe(":4000", mux)
