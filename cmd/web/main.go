5a1d74b406467312fc1aafbb09bf952c85b103ef
Denis-Azarov <denis_azarov@xa.epicgames.com>
2024-08-01 00:28:59 +0100

    code(log): Eliberate handlers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/view", snippetView)
-	mux.HandleFunc("/create", snippetCreate)
+type config struct {
+	addr      *string
+	staticDir *string
+}
 
-	log.Print("start listening on port :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
+func main() {
+	config := config{}
+	config.addr = flag.String("addr", ":4000", "HTTP address to run web server")
+	config.staticDir = flag.String("dir", "./ui/static/", "Path to static assests")
+	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir(*config.staticDir))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/view", app.snippetView)
+	mux.HandleFunc("/create", app.snippetCreate)
+
+	srv := &http.Server{
+		Addr:     *config.addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("start listening on port %s", *config.addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
