b091cafc42f70331c0ab432009048289903bd1a5
Denis-Azarov <i.denis.azarov@gmail.com>
2024-08-11 00:37:21 +0200

    code(middleware): Init code

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
@@ -12,5 +12,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	return app.logRequest(secureHeaders(mux))
 }
