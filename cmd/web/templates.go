1587fad462f3de1849191609d1926cf57c7e1c91
Denis-Azarov <i.denis.azarov@gmail.com>
2024-08-10 19:43:00 +0200

    fix(handlers): Missprint variable name

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -24,17 +24,17 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		name := filepath.Base(page)
 		ts, err := template.ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
-			return nil,err
+		return nil, err
 		}
-		ts, err = template.ParseGlob("./ui/html/partials/*.tmpl")
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
 		if err != nil {
-			return nil,err
+		return nil, err
 		}
-		ts, err = template.ParseFiles(page)
+		ts, err = ts.ParseFiles(page)
 		if err != nil {
-			return nil,err
+		return nil, err
 		}
 		cache[name] = ts
-	}
+		}
 	return cache, nil
 }
