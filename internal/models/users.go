de9d1e4be772929a4f20c67e8df2eb59ef3d008f
Denis-Azarov <i.denis.azarov@gmail.com>
2024-08-16 11:24:04 +0200

    code(cointext): Add initial function

--- internal/models/users.go
+++ internal/models/users.go
@@ -65,5 +65,9 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 	return id, nil
 }
 func (m *UserModel) Exists(id int) (bool, error) {
-	return false, nil
+	var exists bool
+
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	return exists, err
 }
